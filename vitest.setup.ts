51204116499c412e30299cc9e08ea29c540e394e
nnnnusui <nnnnusui@gmail.com>
2024-09-10 15:03:13 +0000

    :hammer_and_wrench: Clean up the render results after testing.

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,9 @@
 import "@testing-library/jest-dom/vitest";
 import { setProjectAnnotations } from "@storybook/react";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
 
 import * as globalStorybookConfig from "./.storybook/preview";
 
 setProjectAnnotations(globalStorybookConfig);
+afterEach(() => cleanup());
